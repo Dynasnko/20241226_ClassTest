37cf7e83518c2537e240db2f365da46207566ef7
student <student@E60760>
2024-12-26 10:46:00 +0800

    加入專案檔案。

---
+++ 1226/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1226
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Puppy DennisDOG = new Puppy();
+            //設定DennisCar的屬性值.
+            DennisDOG.Id = 1;
+            DennisDOG.Owner = "Dennis";
+            DennisDOG.Description = "真的狗";
+            DennisDOG.Price = 50;
+            DennisDOG.Brand = "土狗";
+            DennisDOG.KG = 20;
+            DennisDOG.GG = true;
+            //類堆..所有屬性都要設.
+
+            //最後,叫用DennisDOG的方法CallMyPuppyName( )印出所有屬性參數.
+            DennisDOG.CallMyPuppyName();
+            Console.WriteLine("\n\n");
+
+            //===============================================
+            //第二個 使用有參數的建構子設一台車MarysCar.
+            //經由建構子函式2 來設定所有屬性
+            Puppy MarysNas = new Puppy(2, "Nas", "疊Q專家", 100000, "納瑟斯", 30, true);
+
+            //最後,叫用 MarysCar的方法PrintAllAttrib( )印出所有屬性參數.
+            MarysNas.CallMyPuppyName();
+        }
+    }
+}
---
+++ 1226/Puppy.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1226
+{
+    internal class Puppy
+    {
+        public int Id { get; set; }  //puppy的ID,車牌
+        public string Owner { get; set; }  //puppy主人的名字
+        public string Description { get; set; } //關於這台puppy描述
+        public double Price { get; set; } //這台puppy價格
+        public string Brand { get; set; } //品種
+        public double KG { get; set; }  //體重
+
+        private bool _GG; //結奓
+        public bool GG
+        {
+            get { return this._GG; }
+            set { this._GG = value; }
+        }
+
+        //建構子函式1
+        public Puppy() { }
+
+        //建構子函式2 
+        public Puppy(int id, string inOwner, string description, double price, string brand, double KKG, bool inGG)
+        {
+            Id = id;
+            Owner = inOwner;
+            Description = description;
+            Price = price;
+            Brand = brand;
+            KG = KKG;
+            GG = inGG;
+
+        }
+
+        public void CallMyPuppyName()
+        {
+            Console.WriteLine("ID is:　" + this.Id.ToString());
+            Console.WriteLine("Owener is :　" + this.Owner.ToString());
+            Console.WriteLine("Description is：　"+this.Description.ToString());
+            Console.WriteLine("Price is：　"+this.Price.ToString());
+            Console.WriteLine("Brand is：　"+this.Brand.ToString());
+            Console.WriteLine("KG is：　"+this.KG.ToString());
+            Console.WriteLine("GG yet?　"+this._GG.ToString());
+            //類推..把所有屬性印出來.
+        }
+    }
+}
